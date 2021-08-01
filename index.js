c80aacf6235b70ae341927d89c95221ec62e1705
Manoj Singh <manojsinghrana467@gmail.com>
2021-08-01 15:20:11 +0530

    View Home Page Using Routes and Controller

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const port = 8000;
 
 const app = express();
+app.use('/', require('./routes'));
+
+app.use(express.static('./assets'));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function (err) {
     if (err) {
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+const homeController = require('../controller/homeController');
+
+router.get('/', homeController.home);
+
+module.exports = router;
